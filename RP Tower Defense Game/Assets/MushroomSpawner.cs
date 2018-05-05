7e07565de319c9a67be103dc062102e82c007f4d
RalucaPortase <Raluca_Portase@yahoo.com>
2018-05-05 22:58:51 +0300

    Add the scene and initial scripts for moving the enemy on the map

---
+++ RP Tower Defense Game/Assets/MushroomSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MushroomSpawner : MonoBehaviour {
+
+	public Transform enemyPrefab;
+	public float timeBetweenWaves = 5.5f;
+	public Transform spawnPoint;
+
+	public Text waveCountdownText;
+
+	private float countdown = 2f;
+	private int waveIndex = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if(countdown <= 0f)
+		{
+			StartCoroutine (SpawnWave ());
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+		waveCountdownText.text = Mathf.Floor(countdown).ToString();
+	}
+
+	IEnumerator SpawnWave()
+	{
+		waveIndex++;
+		for (int i = 0; i < waveIndex; i++) 
+		{
+			SpawnEnemy ();
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	void SpawnEnemy()
+	{
+		Instantiate (enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
+
---
+++ RP Tower Defense Game/Assets/prefab/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int waveportIndex = 0;
+
+	void Start () {
+		target = Wavepoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+	
+		if (Vector3.Distance (transform.position, target.position) <= 0.4f)
+		{
+			GetNextWaypoint ();
+		}
+	}
+
+	void GetNextWaypoint()
+	{
+		if (waveportIndex >= Wavepoints.points.Length - 1)
+		{
+			Destroy (gameObject);
+			return;
+		}
+		waveportIndex++;
+		target = Wavepoints.points [waveportIndex];
+	}
+}
